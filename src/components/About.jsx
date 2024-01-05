3f7efdfbf2175b68f109a913de756ea41579ca10
Gaurav <shivabhai7084@gmail.com>
2024-01-05 11:08:01 +0530

    chnage some text and also add google tag manager

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div name="about" className='w-full h-full bg-gradient-to-b from-gray-800 to-black text-white'>
-            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
-                </div>
-                <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero minima eligendi molestias expedita dicta fugit inventore sint, nam, quidem consequatur ipsa quod enim fugiat rerum. Repudiandae voluptatum, quia quam repellat distinctio maiores aut repellendus harum tenetur libero deserunt modi amet voluptate ex inventore quidem id ea a explicabo saepe eos.</p>
-                <br />
-                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, consequuntur! Nemo tempora eos voluptate excepturi ea voluptas aspernatur eveniet delectus ipsa possimus quis laboriosam dolore aperiam illo, odit soluta, quos pariatur, temporibus corrupti doloremque? Fugit, quas dolor. Animi natus debitis dolores delectus corrupti, magni temporibus ipsum autem, libero velit laborum?</p>
-            </div>
-
+  return (
+    <div
+      name="about"
+      className="w-full h-full bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-    )
-}
+        <p className="text-xl mt-20">
+          As a seasoned web developer, I bring a wealth of experience across a
+          spectrum of technologies. My proficiency in front-end development is
+          underscored by mastery in crafting responsive interfaces using HTML,
+          CSS, and popular frameworks like Bootstrap and Tailwind CSS.
+        </p>
+        <br />
+        <p className="text-xl">
+          My expertise extends to dynamic front-end libraries such as React and
+          Vue, where I excel in building interactive components that elevate
+          user experiences. On the server side, I leverage Node.js and Express
+          to develop robust and scalable applications.
+        </p>
+        <br />
+        <p className="text-xl">
+          Database management is a forte, encompassing both relational databases
+          like SQL, where I optimize structures for efficient data handling, and
+          NoSQL solutions like MongoDB, adding versatility to my skill set.
+        </p>
+        <br />
+        <p className="text-xl">
+          My portfolio includes diverse projects, showcasing end-to-end
+          development capabilities. The pursuit of innovation remains a
+          constant, with a commitment to staying current with industry trends
+          and emerging technologies, ensuring a holistic approach to web
+          development challenges.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,55 @@
-import React from 'react'
+import React from "react";
 import HeroImage from "../assets/heroImage.jpeg";
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
-import { Link } from 'react-scroll';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
-    return (
-        <div
-            name="home"
-            className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
-        >
-            <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-                <div className="flex flex-col justify-center h-full">
-                    <h2 className="text-4xl sm:text-6xl font-bold text-white">
-                        I'm a Full Stack Developer
-                    </h2>
-                    <p className="text-gray-500 py-4 max-w-md">
-                        As a fresher building and designing web applications, I am enthusiastic about working with technologies such as React, Tailwind, Next.js, and GraphQL to create innovative and user-friendly web applications.
-                    </p>
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-6xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+            I've been exploring web development and love working with
+            technologies like React, Tailwind, Next.js, and GraphQL. In the last
+            six months, I've learned Vue.js, delved into React.js and
+            JavaScript, and also improved my skills in SQL, Node.js, and
+            Express. I've built various projects to demonstrate my abilities.
+            Right now, I'm working on different projects at my job, enjoying the
+            teamwork, and always trying to come up with new and user-friendly
+            ideas.
+          </p>
 
-                    <div>
-                        <Link
-                            to="portfolio"
-                            smooth
-                            duration={500}
-                            className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
-                        >
-                            Portfolio
-                            <span className="group-hover:rotate-90 duration-300">
-                                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-                            </span>
-                        </Link>
-                    </div>
-                </div>
-
-                <div>
-                    <img
-                        src={HeroImage}
-                        alt="my profile"
-                        className="rounded-2xl mx-auto w-2/3 md:w-93"
-                    />
-                </div>
-            </div>
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
         </div>
-    )
-}
 
-export default Home
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-93"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
